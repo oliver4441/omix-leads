ff98f521534d4e380e515fa9af90d2fde9f76060
Oliver <173495825+oliver4441@users.noreply.github.com>
2026-08-16 18:30:47 +0300

    Turn omix-leads into OMIX Journal shell

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,6 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
-import BusinessAudit from './pages/BusinessAudit'
-import QuoteCalculator from './pages/QuoteCalculator'
-import DealAlerts from './pages/DealAlerts'
-import SellOnOmix from './pages/SellOnOmix'
-import ReferralPage from './pages/ReferralPage'
-import AdminLogin from './pages/AdminLogin'
-import AdminDashboard from './pages/AdminDashboard'
 
 function ScrollToTop() {
   const { pathname } = useLocation()
@@ -19,19 +12,12 @@ function ScrollToTop() {
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-white selection:bg-brand/20 selection:text-brand-dark">
+    <div className="min-h-screen flex flex-col bg-[#f7f8fa] text-slate-900">
       <ScrollToTop />
       <Navbar />
-      <main className="flex-grow animate-fade-in">
+      <main className="flex-grow">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/audit" element={<BusinessAudit />} />
-          <Route path="/quote" element={<QuoteCalculator />} />
-          <Route path="/deals" element={<DealAlerts />} />
-          <Route path="/sell" element={<SellOnOmix />} />
-          <Route path="/referral" element={<ReferralPage />} />
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin/*" element={<AdminDashboard />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </main>
       <Footer />
